c559b50fb4a793efddde83bf5b878af3bd22b80f
Mark Mulder <mark.mulder@unikrn.com>
2025-01-18 13:58:26 +0100

    save user debug

--- src/models/user.ts
+++ src/models/user.ts
@@ -10,6 +10,8 @@ export const saveBatch = async(usersResponse, workspace) => {
 }
 
 export const saveUser = async(user, workspace) => {
-    return await addedit('user', {uid:user.id, workspace, team_id:user.team_id, name:user.name, real_name:user.real_name, is_bot:user.is_bot}, 'uid', 'add', ['uid'] );
+    const payload = {uid:user.id, workspace, team_id:user.team_id, name:user.name, real_name:user.real_name, is_bot:user.is_bot};
+    console.log("SAVING USER", payload);
+    return await addedit('user', payload, 'uid', 'add', ['uid'] );
 }
 
